5c8333e4ab0c6ffc20bc76915bfb6ab33a58882d
Zoya <zmcdonne@uci.edu>
2025-10-27 21:27:58 -0700

    in class code

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public class DeerW5 : MonoBehaviour
+{
+{
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private Transform destenation;
+
+    
+    private int _moveSpeed = 5;
+
+    # cat object in there so it walks twords the cat 
+    
+    private void Update()
+    {
+        _transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+    }
+}
+    [SerializeField] private float _moveSpeed = 1.0f;
+    [SerializeField] private float _turnSpeed = 1.0f;
+
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private Animator _animator;
+}
+
 // Write your DeerW5 class in here :)
 // Hint: if you don't remember what a class is supposed to look like,
 //      maybe check out CatW5...
